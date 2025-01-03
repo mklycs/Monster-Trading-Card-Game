f9cd66df253234d89aadb15aee5b9c44f28ab27b
mklycs <michael.klys@gmail.com>
2025-01-03 21:48:18 +0100

    fixed typo

--- Monster Trading Card Game/presentation-layer/Program.cs
+++ Monster Trading Card Game/presentation-layer/Program.cs
@@ -5,7 +5,7 @@ namespace mtcg{
     internal class Program{
         static void Main(string[] args){
             Server server = new Server();
-            server.Start();
+            server.start();
         }
     }
 }
